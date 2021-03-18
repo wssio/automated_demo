2c546e9d1cf40aa06272488f61e8ee8e4caac4b5
wssio <80741127+wssio@users.noreply.github.com>
2021-03-19 04:11:12 +0800

    Update confs.py

--- keyword_demo/conf/confs.py
+++ keyword_demo/conf/confs.py
@@ -8,6 +8,7 @@ effect :  配置参数
 # driver:选择浏览器驱动
 driver = 'Chrome'
 # login_url:配置登录网页URL
-login_url = 'http://www.duobeiyun.com/login'
+# ******出于保护前公司业务隐私的目的，故此链接已更改为错误链接******
+login_url = 'http://www.baidu.com'
 # login_path_keyword:配置完全关键字驱动的Excel
 login_path_keyword = ('../data/login.xlsx', 'Sheet1')
